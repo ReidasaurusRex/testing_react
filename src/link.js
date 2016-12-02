480eec3ab0d4fb53a3d85437650a101b93eaad88
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-02 12:51:44 -0800

    Added render function to Link component

--- src/link.js
+++ src/link.js
@@ -12,13 +12,32 @@ class Link extends Component {
 
     this._onMouseEnter = this._onMouseEnter.bind(this);
     this._onMouseLeave = this._onMouseLeave.bind(this);
+
+    this.state = {
+      class: STATUS.NORMAL,
+    };
   }
 
-  this.state = {
-    class: STATUS.NORMAL,
-  };
+  _onMouseEnter() {
+    this.setState({class: STATUS.HOVERED});
+  }
+
+  _onMouseLeave() {
+    this.setState({class: STATUS.NORMAL});
+  }
+
+  render() {
+    return (
+      <a
+        className={this.state.class}
+        href={this.props.page || "#"}
+        onMouseEnter={this._onMouseEnter}
+        onMouseLeave={this._onMouseLeave}>
+        {this.props.children}
+      </a>
+    );
+  }
+  
 }
-
-
 
 export default Link;
