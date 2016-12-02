d9be77afe598330ba535fcd3a4ec1a71f212d50b
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-02 12:44:13 -0800

    Added empty Link component with STATUS constant and constructor with super

---
+++ src/link.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const STATUS = {
+  NORMAL: 'normal', 
+  HOVERED: 'hovered',
+};
+
+class Link extends Component {
+
+  constructor() {
+    super();
+  }
+
+}
+
+export default Link;
