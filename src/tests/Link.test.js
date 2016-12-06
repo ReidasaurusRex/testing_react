bc25b87f03c4b97830ecb0a88cbee744876e08b5
ReidasaurusRex <reidhlewis91@gmail.com>
2016-12-06 10:52:56 -0800

    Changed import of App on app.test.js due to new tests directory. Ran tests, creating link snapshot

--- src/tests/Link.test.js
+++ src/tests/Link.test.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import Link from '../src/link';
+import Link from '../link';
 import renderer from 'react-test-renderer';
 
 test('Link changes the class when hovered', () => {
   const linkComponent = renderer.create(
     <Link page="https://www.facebook.com">Facebook</Link>
   );
-  let tree = component.toJSON();
+  let tree = linkComponent.toJSON();
   expect(tree).toMatchSnapshot();
 
   // Triggering callback
   tree.props.onMouseEnter();
   // re-render
-  tree = component.toJSON();
+  tree = linkComponent.toJSON();
   expect(tree).toMatchSnapshot();
 
   // Triggering callback
   tree.props.onMouseLeave();
   // re-render
-  tree = component.toJSON();
+  tree = linkComponent.toJSON();
   expect(tree).toMatchSnapshot();
 });
